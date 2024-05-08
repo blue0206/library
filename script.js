cf1f14d0632af1e4b2853d12ab3bd364309a883d
Aayush Rai <coolaayushrai@gmail.com>
2024-05-08 14:21:07 +0530

    Create Book object constructor function and an array
    The array myLibrary will be used to store objects created from the
    Book object constructor.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+//Array to store books.
+const myLibrary = [];
+
+//Book object constructor function
+function Book(title, author, pages, read)
+{
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+
 // Show modal dialog for adding books upon clicking the
 // 'New Book' button
 const newBook = document.querySelector('button.new-book');
