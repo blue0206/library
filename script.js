b1452d552524d2af89658f8dead2cb709248bb66
Aayush Rai <coolaayushrai@gmail.com>
2024-05-08 12:46:31 +0530

    Add functions for opening and closing modal dialog

---
+++ script.js
@@ -0,0 +1,13 @@
+// Show modal dialog for adding books upon clicking the
+// 'New Book' button
+const newBook = document.querySelector('button.new-book');
+const dialog = document.querySelector('dialog');
+newBook.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+//Function for closing the dialog upon clicking the 'Cancel' button.
+const cancelBtn = document.querySelector('.cancel-btn');
+cancelBtn.addEventListener('click', () => {
+    dialog.close();
+});
